7f5e46cc707abb959b8d6df2bffe4f48c1e4e2e2
Safwanach2002 <146156609+Safwanach2002@users.noreply.github.com>
2025-02-04 01:17:02 +0530

    Update settings.py for static files

--- EPI_Project/settings.py
+++ EPI_Project/settings.py
@@ -121,11 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+# The URL for serving static files
+STATIC_URL = '/static/'
 
-MEDIA_ROOT=BASE_DIR/'media'
-MEDIA_URL='/media/'
+# Directory to collect static files into for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collected files will be stored here
+
+# Directories for development static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Your project’s static directory
+
+# Media files
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
